385acc4a77abed6228f7e3f285443adf4bd82ae5
shanCW <792878578@qq.com>
2019-07-13 13:45:32 +0800

    create linked-list folder

---
+++ leetcode/linked-list/delete-node-in-a-linked-list.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+  if(node.next.val == 5) node.next = node.next.next
+};
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
