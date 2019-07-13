1b5bf106f1b9df2eb64188ad0de3915a60f91efc
shanCW <792878578@qq.com>
2019-07-13 17:02:29 +0800

    source code add reverse method

--- leetcode/linked-list/sourceCode.js
+++ leetcode/linked-list/sourceCode.js
@@ -40,6 +40,17 @@ class NodeList {  //链表 接收数组初始化
     strList.push(JSON.stringify(curNode.val))
     return strList.join('->')
   }
+  reverse(){
+    let prev = null;
+    let curr = this.head;
+    while(curr!==null){
+      let tempCurr = curr.next;
+      curr.next = prev; //next 指向prev指向的内存段
+      prev = curr;//prev指向curr指向的内存段
+      curr = tempCurr//curr指向下一个内存段
+    }
+    return prev
+  }
 }
 
 // let myList = new NodeList([1,2,3,4])
