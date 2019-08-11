31e716694363377cd2888289f883642d20834705
shanCW <792878578@qq.com>
2019-08-11 16:44:14 +0800

    滑动窗口

--- 数据结构与算法/leetcode/String/无重复最长子串.js
+++ 数据结构与算法/leetcode/String/无重复最长子串.js
@@ -1,40 +1,29 @@
-// var lengthOfLongestSubstring = function(string) {
-
-//   let hashTemp = {}
-//   let origin_arr = string.split('')
-//   let maxSubStr = ''
-//   let change_str = origin_arr.reduce((prev,cur,index,arr)=>{
-//       if(index>0){
-//           if(hashTemp[cur]!==undefined){
-//               hashTemp = {}
-//               hashTemp[cur] = index
-//               return prev+','+cur
-//           }else{
-//               hashTemp[cur] = index
-//               return prev+cur
-//           }
-//       }else{
-//           hashTemp[cur] = index
-//           return cur
-//       }
-//   },'')
-//   change_str.split(',').forEach(subStr=>{
-//     subStr.length > maxSubStr.length ? maxSubStr = subStr : ''
-//   })
-//   return maxSubStr.length
-// };
-
-var lengthOfLongestSubstring = function (s) {
-  let hashMap = {};
-  let ans = 0;
-  for (let end = 0, start = 0; end < s.length; end++) {
-    if (hashMap[s[end]]) {
-      start = Math.max(hashMap[s[end]], start)//移动start位置到重复元素的下一个
-    }
-    ans = Math.max(ans, end - start + 1);
-    hashMap[s[end]]=end + 1 //保存index为重复元素的下一个
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-07-25 16:41:07
+ * @LastEditTime: 2019-08-11 16:43:53
+ * @LastEditors: Please set LastEditors
+ */
+//滑动窗口法
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+  let window = {};//存放不重复值
+  //初始两个指针，用来指向窗户的左右两侧
+  let prev = 0;
+  let end = 0 ; 
+  let maxLen = 0
+  //对字符串进行一次遍历，存放非重复的值，如果有重复，则找到最左侧的重复元素，将左指针跳到下一个.并且对当前长度和已经存储的进行大小判断。怎么删除重复节点之前的值？
+  while(end<s.length && prev<s.length){
+      if(!window[s[end]]){
+          window[s[end++]] = true
+          maxLen = Math.max(maxLen,end-prev)
+      }else{
+          delete window[s[prev++]]//删除重复节点之前的值
+      }
   }
-  return ans;
+  return maxLen
 };
-lengthOfLongestSubstring('dvdf')
-
