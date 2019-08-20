6819e3e644c69731bae398b7ae68e74fb9b506d0
shanCW <792878578@qq.com>
2019-08-21 00:07:33 +0800

    real Promise

--- rebuild-knowledge-map/es6/promise/base-usage.js
+++ rebuild-knowledge-map/es6/promise/base-usage.js
@@ -1,10 +1,17 @@
 function wait(ms){
-  return new Promise((resoved,rejece)=>{
+  return new Promise((resolved,rejece)=>{
     //async function
-    setTimeout(resoved,ms,'done')//resolved time resolved的参数
+    setTimeout(()=>{
+      resolved('done')
+    },ms)//resolved time resolved的参数
     //resolved 的作用：将pending状态改为resolved状态，并且调用回调函数this.rejectedCallbacks
   })
 }
 
-console.log(wait(100))
-wait(100).then(res=>console.log(res))//expect:done
+let result = wait(100)//返回Promise 构造函数本身
+let test = wait(100).then('asd')
+
+
+console.log(result)
+console.log(test)
+// wait(100).then(res=>console.log(res))//expect:done-
--- rebuild-knowledge-map/es6/promise/promise-source.js
+++ rebuild-knowledge-map/es6/promise/promise-source.js
@@ -1,62 +1,150 @@
-//手写promise
-// 构造函数Mypromise 
-// 在异步没有成功之前返回Promise { pending }
-// 在成功之后通过.then 调用 返回异步值
+/**
+ * 1. new Promise时，需要传递一个 executor 执行器，执行器立刻执行
+ * 2. executor 接受两个参数，分别是 resolve 和 reject
+ * 3. promise 只能从 pending 到 rejected, 或者从 pending 到 fulfilled
+ * 4. promise 的状态一旦确认，就不会再改变
+ * 5. promise 都有 then 方法，then 接收两个参数，分别是 promise 成功的回调 onFulfilled, 
+ *      和 promise 失败的回调 onRejected
+ * 6. 如果调用 then 时，promise已经成功，则执行 onFulfilled，并将promise的值作为参数传递进去。
+ *      如果promise已经失败，那么执行 onRejected, 并将 promise 失败的原因作为参数传递进去。
+ *      如果promise的状态是pending，需要将onFulfilled和onRejected函数存放起来，等待状态确定后，再依次将对应的函数执行(发布订阅)
+ * 7. then 的参数 onFulfilled 和 onRejected 可以缺省
+ * 8. promise 可以then多次，promise 的then 方法返回一个 promise
+ * 9. 如果 then 返回的是一个结果，那么就会把这个结果作为参数，传递给下一个then的成功的回调(onFulfilled)
+ * 10. 如果 then 中抛出了异常，那么就会把这个异常作为参数，传递给下一个then的失败的回调(onRejected)
+ * 11.如果 then 返回的是一个promise,那么需要等这个promise，那么会等这个promise执行完，promise如果成功，
+ *   就走下一个then的成功，如果失败，就走下一个then的失败
+ */
+
 const PENDING = 'pending';
-const RESOLVED = 'resolved';
-const REJECT = 'rejected';
+const FULFILLED = 'fulfilled';
+const REJECTED = 'rejected';
+function Promise(executor) {
+    let self = this;
+    self.status = PENDING;
+    self.onFulfilled = [];//成功的回调
+    self.onRejected = []; //失败的回调
+    //PromiseA+ 2.1
+    function resolve(value) {
+        if (self.status === PENDING) {
+            self.status = FULFILLED;
+            self.value = value;
+            self.onFulfilled.forEach(fn => fn());//PromiseA+ 2.2.6.1
+        }
+    }
 
-function MyPromise(fn) {
-  this.state = PENDING;
-  this.value = null;
-  this.resolvedCallbacks = [];//?：为什么要用数组保存
-  this.rejectedCallbacks = [];
-  this.resolved = resolved.bind(this)//返回一个绑定上下文的resolved函数
-  this.reject = resolved.bind(this)
+    function reject(reason) {
+        if (self.status === PENDING) {
+            self.status = REJECTED;
+            self.reason = reason;
+            self.onRejected.forEach(fn => fn());//PromiseA+ 2.2.6.2
+        }
+    }
 
-  //实现如何执行 `Promise` 中传入的函数 ?
-  try {
-    fn(this.resolved, this.reject)
-  } catch (e) {
-    this.reject(e)
-  }
-
+    try {
+        executor(resolve, reject);
+    } catch (e) {
+        reject(e);
+    }
 }
 
-function resolved(value) {
-
-  if (this.state !== PENDING) return
-  this.state = RESOLVED;
-  this.value = value;
-  this.resolvedCallbacks.map(cb => cb(this.value))
+Promise.prototype.then = function (onFulfilled, onRejected) {
+    //PromiseA+ 2.2.1 / PromiseA+ 2.2.5 / PromiseA+ 2.2.7.3 / PromiseA+ 2.2.7.4
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
+    onRejected = typeof onRejected === 'function' ? onRejected : reason => { throw reason };
+    let self = this;
+    //PromiseA+ 2.2.7
+    let promise2 = new Promise((resolve, reject) => {
+        if (self.status === FULFILLED) {
+            //PromiseA+ 2.2.2
+            //PromiseA+ 2.2.4 --- setTimeout
+            setTimeout(() => {
+                try {
+                    //PromiseA+ 2.2.7.1
+                    let x = onFulfilled(self.value);
+                    resolvePromise(promise2, x, resolve, reject);
+                } catch (e) {
+                    //PromiseA+ 2.2.7.2
+                    reject(e);
+                }
+            });
+        } else if (self.status === REJECTED) {
+            //PromiseA+ 2.2.3
+            setTimeout(() => {
+                try {
+                    let x = onRejected(self.reason);
+                    resolvePromise(promise2, x, resolve, reject);
+                } catch (e) {
+                    reject(e);
+                }
+            });
+        } else if (self.status === PENDING) {
+            self.onFulfilled.push(() => {
+                setTimeout(() => {
+                    try {
+                        let x = onFulfilled(self.value);
+                        resolvePromise(promise2, x, resolve, reject);
+                    } catch (e) {
+                        reject(e);
+                    }
+                });
+            });
+            self.onRejected.push(() => {
+                setTimeout(() => {
+                    try {
+                        let x = onRejected(self.reason);
+                        resolvePromise(promise2, x, resolve, reject);
+                    } catch (e) {
+                        reject(e);
+                    }
+                });
+            });
+        }
+    });
+    return promise2;
 }
 
-function reject(value) {
-  if (this.state !== PENDING) return;
-  this.state = REJECT;
-  this.value = value;
-  this.rejectedCallbacks.map(cb => cb(this.value))
-}
-/***********then实现（对回调函数进行执行，如果当前状态是pending则将回调函数入栈）********** */
-MyPromise.prototype.then = function (onFulfilled, onRejected) {
-  onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : v => v
-  onRejected = typeof onRejected === 'function' ? onRejected :r => {throw r}
-  // console.log(this)
-  switch (this.state){
-    case PENDING :
-      this.resolvedCallbacks.push(onFulfilled);
-      this.rejectedCallbacks.push(onRejected)
-      break;
-    case RESOLVED : onFulfilled(this.value);break;
-    case REJECT : onRejected(this.value);break;
-  }
+function resolvePromise(promise2, x, resolve, reject) {
+    let self = this;
+    //PromiseA+ 2.3.1
+    if (promise2 === x) {
+        reject(new TypeError('Chaining cycle'));
+    }
+    if (x && typeof x === 'object' || typeof x === 'function') {
+        let used; //PromiseA+2.3.3.3.3 只能调用一次
+        try {
+            let then = x.then;
+            if (typeof then === 'function') {
+                //PromiseA+2.3.3
+                then.call(x, (y) => {
+                    //PromiseA+2.3.3.1
+                    if (used) return;
+                    used = true;
+                    resolvePromise(promise2, y, resolve, reject);
+                }, (r) => {
+                    //PromiseA+2.3.3.2
+                    if (used) return;
+                    used = true;
+                    reject(r);
+                });
+
+            }else{
+                //PromiseA+2.3.3.4
+                if (used) return;
+                used = true;
+                resolve(x);
+            }
+        } catch (e) {
+            //PromiseA+ 2.3.3.2
+            if (used) return;
+            used = true;
+            reject(e);
+        }
+    } else {
+        //PromiseA+ 2.3.3.4
+        resolve(x);
+    }
 }
 
-function delay(ms) {
-  return new MyPromise((resolved, reject) => {
-    //需要bind将resolved的上下文变为MyPromise
-    setTimeout(resolved, ms, 'done')
-  })
-}
-console.log(delay(100))
-delay(100).then(res=>console.log(res))
+module.exports = Promise;
+
