3a6cc01271e89af65c6965b646592c4832b2dcc1
shanCW <792878578@qq.com>
2019-08-22 11:30:55 +0800

    fix

--- 数据结构与算法/DP/leastcostClimb.js
+++ 数据结构与算法/DP/leastcostClimb.js
@@ -1,9 +1,13 @@
-var minCostClimbingStairs = function(cost,n=cost.length,memo= {}) {
+var minCostClimbingStairs = function(cost) {
+  let copy = [...cost,0]
+  return DP(copy)
+};
+function DP(cost,n=cost.length-1,memo= {}){
   if(memo[n]) return memo[n]
   if(n==0) return cost[0]
   if(n==1) return cost[1]
-  else memo[n] = Math.min(minCostClimbingStairs(cost,n-1,memo),minCostClimbingStairs(cost,n-2,memo))+cost[n]
+  else memo[n] = Math.min(DP(cost,n-1,memo),DP(cost,n-2,memo))+cost[n]
   return memo[n]
-};
-
-minCostClimbingStairs([0,0,0,0])
+}
+let res = minCostClimbingStairs([0,0,0,0,2,1])
+console.log(res)
