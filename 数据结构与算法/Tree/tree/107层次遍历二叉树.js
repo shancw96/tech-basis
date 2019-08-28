66fc54b838969f84017f08d215a1f17d31175fda
shanCW <792878578@qq.com>
2019-08-28 16:52:41 +0800

    update

--- 数据结构与算法/Tree/tree/107层次遍历二叉树.js
+++ 数据结构与算法/Tree/tree/107层次遍历二叉树.js
@@ -9,23 +9,21 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-var levelOrder = function(root,stack = [],res = [],depth = 0) {
+var levelOrderBottom = function(root,depth=0,res = [],stack = []) {
   if(root == null) return []
   stack.push(root)
   while(stack.length){
+      res[depth] = []
       let curLayer = stack.length;
       while(curLayer){
           curLayer -= 1;
-          let cur = stack.shift()
-          //动态初始化二维数组
-          if( !(res[depth] instanceof Array)) res[depth] = new Array()
-          res[depth].push(cur.val)
-          //子节点入栈
-          cur.left ? stack.push(cur.left) :''
-          cur.right ? stack.push(cur.right) : ''
+          let curNode = stack.shift()
+          res[depth].push(curNode.val)
+          //存储左右节点
+          curNode.left ? stack.push(curNode.left):''
+          curNode.right ? stack.push(curNode.right):''
       }
-      //当前层执行完毕,深度+1
-      depth+=1
+      depth += 1
   }
-  return res
+  return res.reverse()
 };
