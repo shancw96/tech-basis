be60e9608dcef3e4690becf1ba3e3a7dc8a52d5e
shanCW <792878578@qq.com>
2019-09-03 22:51:10 +0800

    update

--- rebuild-knowledge-map/复习/对象/deepclone3.js
+++ rebuild-knowledge-map/复习/对象/deepclone3.js
@@ -1,21 +1,22 @@
-function deepClone(obj,dictionary = new WeakMap()){
+function deepclone(obj,dictionary = new WeakMap()){//去除环
 
   if(typeof obj !== 'object') return obj
   if(dictionary.has(obj)) return dictionary.get(obj)
   let cloneObj = obj instanceof Array ? [] : {}
   dictionary.set(obj,cloneObj)
   Object.keys(obj).forEach(key=>{
-    cloneObj[key] = deepClone(obj[key],dictionary)
+    cloneObj[key] = deepclone(obj[key],dictionary)
   })
   return cloneObj
 }
+
 let obj = {
   a:1,
-  b:2,
-  c:{
-    a:1
+  b:{
+    a:2,
+    c:3
   }
 }
 obj.a = obj
-let res = deepClone(obj)
-console.log(res)
+
+let ans = deepclone(obj)
