e31426f722289d813f4cacd283126f05044c1f17
shangchengw <792878578@qq.com>
2020-01-02 16:12:36 +0800

    then implement

--- rebuild-knowledge-map/es6/promise/implement/source_then_implement.js
+++ rebuild-knowledge-map/es6/promise/implement/source_then_implement.js
@@ -52,6 +52,7 @@ function MyPromise(fn) {
     }
   }
 
+  //不管结果是什么，promise 还是plain value 都直接执行结果
   //将resolve 与 reject 传递给handle方法，handle根据状态来决定使用那个方法
   this.done = function(onFulfilled, onRejected) {
     //确保一定是下一轮 时间轮片开始才执行。这里参考100行左右的代码
@@ -60,6 +61,40 @@ function MyPromise(fn) {
       handle({ onFulfilled, onRejected });
     }, 0);
   };
+
+  //在done 基础上增加了判断 done结果是否为promise
+  this.then = function(onFulfilled, onRejected) {
+    const curPromise = this;
+    return new Promise((resolve, reject) => {
+      //new Promise -> doResolve ---（run fn)---> curPromise.done
+      return curPromise.done(
+        result => {
+          if (typeof onFulfilled === "function") {
+            //promise
+            try {
+              return resolve(onFulfilled(result));
+            } catch (ex) {
+              return reject(ex);
+            }
+          } else {
+            return resolve(result);
+          }
+        },
+        err => {
+          if (typeof onRejected === "function") {
+            try {
+              return resolve(onRejected(err));
+            } catch (ex) {
+              return reject(ex);
+            }
+          } else {
+            return reject(error);
+          }
+        }
+      );
+    });
+  };
+
   //fn -> (resolve,rejected)=>{...}
   doResolve(fn, resolve, rejected); //将上面的resolve 和 reject 函数传入fn中，为什么不用curry？
 }
