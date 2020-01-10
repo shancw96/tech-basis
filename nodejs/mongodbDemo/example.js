b81319eb8f9c15fe04db253a38d65bbbd1eb945b
shanCW <792878578@qq.com>
2020-01-10 22:33:05 +0800

    mongoose usage test

---
+++ nodejs/mongodbDemo/example.js
@@ -0,0 +1,16 @@
+const { handler, close } = require("./index");
+
+// handler
+//     .save({
+//         userName: "shancw3",
+//         passwd: "9527",
+//         location: "huaian",
+//         books: [{ title: "遮天" }]
+//     })
+//     .then(res => {
+//         console.log(res);
+//         close();
+//     });
+// handler.update({ userName: "shancw2" }, { location: "淮安" }).then(res => console.log(res));
+// handler.remove({ location: "huaian" }).then(res => console.log(res));
+handler.findAll({ userName: "shancw2" }).then(res => console.log(res));
