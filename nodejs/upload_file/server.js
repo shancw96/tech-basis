7128441eb23b6dead066bd2d4a4f85439decf6ee
shanCW <792878578@qq.com>
2020-01-21 21:48:38 +0800

    upload file

--- nodejs/upload_file/server.js
+++ nodejs/upload_file/server.js
@@ -1,19 +1,35 @@
 const Koa = require("koa");
 const Router = require("koa-router");
-
+const cors = require("koa2-cors");
+const stdout = require("shancw-stdout");
+const bodyParser = require("koa-body");
 const app = new Koa();
 const router = new Router();
 
 router.post("/uploadChunk", ctx => {
-  ctx.body = "connect";
+    stdout.bgGreen("handle upload file");
+    console.log(ctx.request.body);
+    ctx.body = "connect";
 });
 
 router.post("/mergeChunk", ctx => {
-  ctx.body = "connect";
+    stdout.bgGreen("handle upload file");
+    console.log(ctx.request.body);
+    ctx.body = "connect";
 });
 
 app.listen(4007, () => {
-  console.log("file upload server start at localhost:4008");
+    console.log("file upload server start at localhost:4007");
 });
 
-app.use(router.routes()).use(router.allowedMethods());
+app.use(cors())
+    .use(
+        bodyParser({
+            multipart: true,
+            formidable: {
+                maxFileSize: 200 * 1024 * 1024 // 设置上传文件大小最大限制，默认2M
+            }
+        })
+    )
+    .use(router.routes())
+    .use(router.allowedMethods());
