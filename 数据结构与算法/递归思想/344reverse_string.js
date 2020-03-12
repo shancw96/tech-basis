ae840b860d5e654b7446457d66da5ba31d8a00fb
shancw96 <shancw1996@gmail.com>
2020-03-13 02:01:09 +0800

    add recursion helper

--- 数据结构与算法/递归思想/344reverse_string.js
+++ 数据结构与算法/递归思想/344reverse_string.js
@@ -1,15 +1,24 @@
-var reverseString = function(s) {
+const logBy = require('../../utils/hoc/recursionLog')
+const logStr = logBy((skipedTab,str)=>{
+  console.log(skipedTab+str)
+})
+const logUseless = logBy((skipedTab,useless)=>{
+  console.log(skipedTab+useless)
+})
+var reverseString = function(s,depth=0) {
   if(s.length <= 0) return s
-  return  s[s.length-1]+reverseString(s.slice(1,s.length-1))+s[0]
+  logStr(depth,s)
+  logUseless(depth,'附加信息')
+  return  s[s.length-1]+reverseString(s.slice(1,s.length-1),depth+1)+s[0]
 };
+reverseString('123')
 
-
-/**
- * @param {character[]} s
- * @return {void} Do not return anything, modify s in-place instead.
- */
-var reverseString = function(s,start_p=0,end_p=s.length-1) {
-  while(start_p<end_p){
-      [s[start_p++],s[end_p--]] = [s[end_p],s[start_p]]
-  }
-};
+// /**
+//  * @param {character[]} s
+//  * @return {void} Do not return anything, modify s in-place instead.
+//  */
+// var reverseString = function(s,start_p=0,end_p=s.length-1) {
+//   while(start_p<end_p){
+//       [s[start_p++],s[end_p--]] = [s[end_p],s[start_p]]
+//   }
+// };
