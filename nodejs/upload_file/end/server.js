af37c26686b61aaf8eb75c39590ddb092ea597ab
shancw96 <shancw1996@gmail.com>
2020-03-19 17:31:30 +0800

    done upload file demo

--- nodejs/upload_file/end/server.js
+++ nodejs/upload_file/end/server.js
@@ -5,18 +5,40 @@ const cors = require("koa2-cors");
 const KoaBody = require('koa-body')
 const Router = require("koa-router");
 const stdout = require("shancw-stdout");
+const {renameFile,mergeChunkFile} = require('./utils')
 const path = require('path')
 const PORT = 8081;
 const server = new Koa();
 const router = new Router()
+
+const uploadChunkPath = path.resolve(__dirname,'../data')
+
 server.listen(PORT, () => {
     stdout.bgGreen(`server start at port:${PORT}`);
 });
 
 router.post('/upload',ctx=>{
-    console.log(ctx.request.body)
-    console.log(ctx.request.files)//获取chunk
-    ctx.body = 'connect'
+    if(ctx.request.body.type === 'merge'){
+        try{
+            const {token,chunkCount,fileName} = ctx.request.body
+            mergeChunkFile(fileName,uploadChunkPath,chunkCount,token,'../data')    
+            ctx.body =  'ok'
+        }catch(e){
+            ctx.body = "merge fail"
+        }
+    }else if(ctx.request.body.type === 'upload'){
+        try{
+            const {index,token,name} = ctx.request.body
+            const chunkFile = ctx.request.files.chunk
+            const chunkName = chunkFile.path.split('/').pop()
+            renameFile(uploadChunkPath,chunkName,`${name}-${index}-${token}`)
+            ctx.body = 'upload chunk success'
+        }catch(e){
+            ctx.body = 'upload chunk fail'
+        }
+    }else{
+        ctx.body = "unkown type"
+    }
 })
 
 server
@@ -25,7 +47,7 @@ server
         multipart:true, // 支持文件上传 
         formidable: {
             //设置文件的默认保存目录，不设置则保存在系统临时目录下  os
-            uploadDir: path.resolve(__dirname, '../data')
+            uploadDir: uploadChunkPath
         },
     }))
     .use(router.allowedMethods())
