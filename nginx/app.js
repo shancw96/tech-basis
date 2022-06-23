da8c2f8cc2c7e1a8686fa73532f99cfc5763314f
shanCW <shancw1996@gmail.com>
2022-06-23 14:38:24 +0800

    chore: adjuest structure

--- nginx/app.js
+++
@@ -1,8 +0,0 @@
-const Koa = require('koa')
-const app = new Koa()
-
-app.use(ctx => {
-  console.log(ctx.request)
-  ctx.body = 'app'
-})
-app.listen(8082)
--- nginx/blog.js
+++
@@ -1,8 +0,0 @@
-const Koa = require('koa')
-const app = new Koa()
-
-app.use(ctx => {
-  console.log(ctx.request)
-  ctx.body = 'blog'
-})
-app.listen(8081)
