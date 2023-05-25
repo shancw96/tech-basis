a7d359523838ea0e749a8b808f9620a41d3aa1e9
shancw <shancw1996@gmail.com>
2023-05-25 23:47:59 +0800

    update: langchain vectorDB QA

--- implement/Vue3/3.2.47/reactive.js
+++ implement/Vue3/3.2.47/reactive.js
@@ -1,8 +1,14 @@
 // reactive： plain object to proxy object
 // v0.1 只对普通对象进行了代理，支持 get set trap
+
+const proxyMap = new WeakMap()
 function reactive(target) {
   if (Object.prototype.toString.call(target) !== '[object Object]') {
     throw new Error("unsupported type, only support plain object")
+  }
+
+  if (proxyMap.has(target)) {
+    return proxyMap.get(target)
   }
 
   const handler = {
@@ -10,15 +16,20 @@ function reactive(target) {
     set
   }
 
-  return new Proxy(target, handler)
+  const proxyObj = new Proxy(target, handler)
+
+  proxyMap.set(target, proxyObj)
+
+  return proxyObj
 }
 
+// 1. get value
+// 2. set targetMap deps effect relation
 function get(target, key) {
-  console.log("get", key)
-  return Reflect.get(target, key)
+  const res = Reflect.get(target, key)
+  return res
 }
 
 function set(target, key ,value, receiver) {
-  console.log("set", key, value)
-  return Reflect.set(target, key, value, receiver)
+  Reflect.set(target, key, value, receiver)
 }
