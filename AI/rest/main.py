205e184c352a0ebe89df9dc976968b2dd3babb02
shancw <shancw1996@gmail.com>
2023-06-08 14:26:23 +0800

    AI-chatboot-vectordb-chat-retrival-v1

--- AI/rest/main.py
+++ AI/rest/main.py
@@ -1,41 +1,16 @@
-
-from fastapi import FastAPI
-from langchain import LLMChain
 import uvicorn
-
-from langchain.embeddings.openai import OpenAIEmbeddings
-
-from prompts import QA_PROMPT
-from typing import AsyncIterable, Awaitable, Optional
-
-from langchain.chat_models import ChatOpenAI
-from langchain.chains.summarize import load_summarize_chain
-
 import asyncio
 import os
-from typing import AsyncIterable
-
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
-from langchain.callbacks import AsyncIteratorCallbackHandler
-from langchain.schema import HumanMessage
-from pydantic import BaseModel
-from langchain.chat_models import AzureChatOpenAI
 from dotenv import load_dotenv
-
+from fastapi import FastAPI
+from utils import StreamRequest
+from utils import reduce_tokens_below_limit, send_message, createEmbedding, StreamRequest
+from prompts import QA_PROMPT
 from fastapi.middleware.cors import CORSMiddleware
-
 from utils import HistoryLoggerAsyncHandler, get_or_create_chatgroup_vector_db
-
-load_dotenv('/Users/wushangcheng/project/tech-basis/AI/rest/.env')
-
-
-OPENAI_API_BASE = os.getenv('OPENAI_API_BASE')
-OPENAI_API_VERSION = os.getenv('OPENAI_API_VERSION')
-OPENAI_API_TYPE = os.getenv('OPENAI_API_TYPE')
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-
 
 app = FastAPI()
 
@@ -45,106 +20,46 @@ app.add_middleware(
         "http://localhost",
         "http://localhost:1002",
     ],
-    # allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-class StreamRequest(BaseModel):
-    message: str
-
-"""
-    TODO: use custom QA chain refactor send_message function 
-    custom_qa_chain = LLMChain(llm=chatllm, prompt=QA_PROMPT, verbose=True)
-    print("正在发起提问...")
-    res = custom_qa_chain.predict(context = summary, question = payload.prompt)
-"""
-async def send_message(message: str, callbacks = []) -> AsyncIterable[str]:
-    callback = AsyncIteratorCallbackHandler()
     
-    model = AzureChatOpenAI(
-        openai_api_base=OPENAI_API_BASE,
-        openai_api_version=OPENAI_API_VERSION,
-        deployment_name='gpt-35',
-        openai_api_key=OPENAI_API_KEY,
-        openai_api_type=OPENAI_API_TYPE,
-        streaming=True,
-        verbose=True,
-        temperature=0.8,
-        callbacks=[callback]
-    )
+embeddings = createEmbedding("openai")
 
-    async def wrap_done(fn: Awaitable, event: asyncio.Event):
-        """Wrap an awaitable with a event to signal when it's done or an exception is raised."""
-        try:
-            await fn
-        except Exception as e:
-            print(f"Caught exception: {e}")
-        finally: 
-            # Signal the aiter to stop.
-            event.set()
-    # Begin a task that runs in the background.
-    task = asyncio.create_task(wrap_done(
-        model.agenerate(messages=[[HumanMessage(content=message)]]),
-        callback.done),
-    )
-
-    async for token in callback.aiter():
-        # Use server-sent-events to stream the response
-        yield f"{token}"
-    await task
-
-
-class StreamRequest(BaseModel):
-    """Request body for streaming."""
-    prompt: str
-    chat_group_id: str
-    memory_size: Optional[int] = None
-
-
-embeddings = OpenAIEmbeddings(deployment="embedding")
-    
 @app.post("/chat-process")
 def stream(body: StreamRequest):
     """1. create or get VectorDB """
     persist_dir = "store/chat_history"
 
-    db = get_or_create_chatgroup_vector_db(body['chat_group_id'], embeddings, persist_dir)
-    print("正在检索历史聊天记录...")
-    docs = db.similarity_search(query=body['prompt'], k=body['memory_size'] or 10)
-
-    # TODO 裁剪docs，根据docs 的最大的token(限制为1024)
-
-    """2. summarize the docs"""
-    print("正在组织生成记忆...", docs)
-    summaryllm = AzureChatOpenAI(
-        openai_api_base=OPENAI_API_BASE,
-        openai_api_version=OPENAI_API_VERSION,
-        deployment_name='gpt-35',
-        openai_api_key=OPENAI_API_KEY,
-        openai_api_type=OPENAI_API_TYPE,
-    )
+    db = get_or_create_chatgroup_vector_db(body.chat_group_id, embeddings, persist_dir)
     
-    chain = load_summarize_chain(summaryllm, chain_type="stuff")
-    summary = chain.run(docs)
-    print("生成记忆：", summary)
+    print("正在检索历史聊天记录...")
+    docs = reduce_tokens_below_limit(db.similarity_search(query=body.prompt, k=body.memory_size or 50), limit=2048)
+
+    docs_contents = "\n".join([doc.page_content for doc in docs])
 
     """3. reformat the question"""
-    format_question = QA_PROMPT.format_prompt(context=summary, question=body.prompt).to_string()
+    format_question = QA_PROMPT.format_prompt(context=docs_contents, question=body.prompt).to_string()
     print("正在重新组织提问内容...", format_question)
 
     async def update_vectordb_on_llm_end(chatlist = []):
         await asyncio.get_running_loop().run_in_executor(None, db.add_texts, chatlist)
         await asyncio.get_running_loop().run_in_executor(None, db.persist)
-
+    
+    # TODO 云端存储历史记录
     # async def update_mysql_on_llm_end(chatlist = []):
     #     print("TODO: save chatlog to mysql", chatlist)
 
-    logCallback = HistoryLoggerAsyncHandler([update_vectordb_on_llm_end])
-
     """4. QA """
-    return StreamingResponse(send_message(format_question, callbacks=logCallback), media_type="text/event-stream", headers={'content-type': 'application/octet-stream'})
+    return StreamingResponse(
+        send_message(
+            format_question, 
+            extra_callback=HistoryLoggerAsyncHandler([update_vectordb_on_llm_end])
+            ), 
+        media_type="text/event-stream", 
+        headers={'content-type': 'application/octet-stream'}
+        )
 
 
 if __name__ == "__main__":
--- AI/rest/utils.py
+++ AI/rest/utils.py
@@ -1,64 +1,63 @@
 import asyncio
 from typing import Optional, Union, List
 from pydantic import BaseModel
-
 from langchain.vectorstores import Chroma
 import chromadb
 from chromadb.config import Settings
 from typing import Any, Dict, List
 from pydantic import BaseModel, validator
-from langchain.callbacks.base import BaseCallbackHandler, AsyncCallbackHandler
+from langchain.callbacks.base import AsyncCallbackHandler
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.schema import (
-    LLMResult,
-)
-
+from langchain.schema import (LLMResult,)
 from webbrowser import Chrome
 from langchain.document_loaders import DirectoryLoader
-
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
+from langchain.embeddings.openai import OpenAIEmbeddings
+from typing import AsyncIterable, Awaitable, List, Optional
+from langchain.docstore.document import Document
+import asyncio
+import os
+from typing import AsyncIterable
+from langchain.callbacks import AsyncIteratorCallbackHandler
+from langchain.schema import HumanMessage
+from pydantic import BaseModel
+from langchain.chat_models import AzureChatOpenAI
+from dotenv import load_dotenv
+from langchain.embeddings import HuggingFaceEmbeddings
 
 
-class ChatMessage(BaseModel):
-    chat_history: List[str]
-    prompt: str
-    chat_id: Optional[Union[str, None]] = None
+load_dotenv('/Users/wushangcheng/project/tech-basis/AI/rest/.env')
+
+OPENAI_API_BASE = os.getenv('OPENAI_API_BASE')
+OPENAI_API_VERSION = os.getenv('OPENAI_API_VERSION')
+OPENAI_API_TYPE = os.getenv('OPENAI_API_TYPE')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 
-# async def chat_response_stream(prompt, chatllm, chat_id="chat_9527"):
+model = AzureChatOpenAI(
+    openai_api_base=OPENAI_API_BASE,
+    openai_api_version=OPENAI_API_VERSION,
+    deployment_name='gpt-35',
+    openai_api_key=OPENAI_API_KEY,
+    openai_api_type=OPENAI_API_TYPE,
+)
 
-#   db = get_or_create_chatgroup_vector_db(chat_id, "store")
-
-#   print("正在读取与提问相关联的记忆...")
-#   docs = db.similarity_search(query=prompt, k=4)
-#   # refine question
-#   chain = load_summarize_chain(summaryllm, chain_type="stuff")
-#   summary = chain.run(docs)
-#   print("总结上下文如下：", summary)
-
-#   custom_qa_chain = LLMChain(llm=chatllm, prompt=QA_PROMPT, verbose=True)
-#   print("正在调用openai发起提问...")
-#   res = custom_qa_chain.predict(context = summary, question = prompt)
-
-#   print("正在添加聊天记录至记忆库...", ["Human: " + prompt, "Assistant: " + res])
-#   db.add_texts(["Human" + prompt, "Assistant: " + res])
-#   db.persist()
-
-#   return res
 
 # 判断是否存在该聊天分组的矢量数据库，不存在就新建一个
 def get_or_create_chatgroup_vector_db(chat_id, embedding, persist_dir="store"):
     if not is_chroma_collection_exist(chat_id, persist_dir):
         print("正在使用历史聊天记录为当前聊天分组构建矢量数据库...")
         # TODO: retrive chat history from db
-        chat_history = [ "an assistant is glad to help user, and provide useful advice" ]
+        chat_history = [
+            "an assistant is glad to help user, and provide useful advice"]
         vectordb = Chroma.from_texts(
             persist_directory=persist_dir, texts=chat_history, embedding=embedding, collection_name=chat_id)
         vectordb.persist()
         vectordb = None
 
     return Chroma(persist_directory=persist_dir, embedding_function=embedding, collection_name=chat_id)
+
 
 def is_chroma_collection_exist(collection_name, persist_dir="store"):
     try:
@@ -70,24 +69,27 @@ def is_chroma_collection_exist(collection_name, persist_dir="store"):
         return True
     except Exception as e:
         return False
-    
-def get_or_init_docs_collection(collection_name, docs_source_path, embedding, persist_dir):
-  try:
-    if not is_chroma_collection_exist():
-      loader = DirectoryLoader(docs_source_path, glob = "**/*.md")
-      docs = loader.load()
-      print (f'You have {len(docs)} document(s) in your data')
-      text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-      split_docs = text_splitter.split_documents(docs)
-      vectorstore = Chroma.from_documents(split_docs, embedding, persist_directory=persist_dir)
-      vectorstore.persist()
-      print (f'已为当前文档集生成矢量数据')
-      vectorstore = None
 
-    return Chrome(persist_directory=persist_dir, embedding_function=embedding, collection_name=collection_name)
-  except Exception as e:
-    print("####文档矢量数据库加载失败####")
-    print(e)
+
+def get_or_init_docs_collection(collection_name, docs_source_path, embedding, persist_dir):
+    try:
+        if not is_chroma_collection_exist():
+            loader = DirectoryLoader(docs_source_path, glob="**/*.md")
+            docs = loader.load()
+            print(f'You have {len(docs)} document(s) in your data')
+            text_splitter = RecursiveCharacterTextSplitter(
+                chunk_size=1000, chunk_overlap=0)
+            split_docs = text_splitter.split_documents(docs)
+            vectorstore = Chroma.from_documents(
+                split_docs, embedding, persist_directory=persist_dir)
+            vectorstore.persist()
+            print(f'已为当前文档集生成矢量数据')
+            vectorstore = None
+
+        return Chrome(persist_directory=persist_dir, embedding_function=embedding, collection_name=collection_name)
+    except Exception as e:
+        print("####文档矢量数据库加载失败####")
+        print(e)
 
 
 class HistoryLoggerAsyncHandler(AsyncCallbackHandler):
@@ -99,7 +101,7 @@ class HistoryLoggerAsyncHandler(AsyncCallbackHandler):
     async def on_llm_start(
         self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any
     ) -> None:
-        
+
         self.logger.append(prompts[0])
 
     async def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
@@ -117,7 +119,7 @@ class StreamingLLMCallbackHandler(AsyncCallbackHandler):
         self.websocket = websocket
 
     async def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
-        
+
         resp = ChatResponse(sender="bot", message=token, type="stream")
         await asyncio.sleep(0.3)
         print(resp.dict())
@@ -126,6 +128,7 @@ class StreamingLLMCallbackHandler(AsyncCallbackHandler):
 class StreamingStdOutCallbackHandlerYield(StreamingStdOutCallbackHandler):
     def __init__(self, queue):
         self.q = queue
+
     def on_llm_start(
         self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any
     ) -> None:
@@ -144,77 +147,67 @@ class StreamingStdOutCallbackHandlerYield(StreamingStdOutCallbackHandler):
         self.q.put("###finish###")
 
 
-# class StreamingStdOutCallbackHandler(BaseCallbackHandler):
-#     """Callback handler for streaming. Only works with LLMs that support streaming."""
+def createEmbedding(type: str):
+    if type == 'openai':
+        return OpenAIEmbeddings(deployment="embedding", chunk_size=1)
+    # FIXME: huggingface和chromadb 集成报错
+    elif type == 'huggingface':
+        model_name = "sentence-transformers/all-mpnet-base-v2"
+        model_kwargs = {'device': 'cpu'}
+        encode_kwargs = {'normalize_embeddings': False}
+        return HuggingFaceEmbeddings(
+            model_name=model_name,
+            model_kwargs=model_kwargs,
+            encode_kwargs=encode_kwargs
+        )
+    else:
+        return None
 
-#     def on_llm_start(
-#         self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any
-#     ) -> None:
-#         """Run when LLM starts running."""
 
-#     def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
-#         """Run on new LLM token. Only available when streaming is enabled."""
-#         sys.stdout.write(token)
-#         sys.stdout.flush()
+def reduce_tokens_below_limit(docs: List[Document], limit: int = 2048):
+    num_docs = len(docs)
+    tokens = [model.get_num_tokens(doc.page_content) for doc in docs]
+    token_count = sum(tokens[:num_docs])
+    while token_count > limit:
+        num_docs -= 1
+        token_count -= tokens[num_docs]
+    return docs[:num_docs]
 
-#     def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
-#         """Run when LLM ends running."""
 
-#     def on_llm_error(
-#         self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
-#     ) -> None:
-#         """Run when LLM errors."""
+async def send_message(message: str, extra_callback) -> AsyncIterable[str]:
+    callback = AsyncIteratorCallbackHandler()
 
-#     def on_chain_start(
-#         self, serialized: Dict[str, Any], inputs: Dict[str, Any], **kwargs: Any
-#     ) -> None:
-#         """Run when chain starts running."""
+    model = AzureChatOpenAI(
+        openai_api_base=OPENAI_API_BASE,
+        openai_api_version=OPENAI_API_VERSION,
+        deployment_name='gpt-35',
+        openai_api_key=OPENAI_API_KEY,
+        openai_api_type=OPENAI_API_TYPE,
+        streaming=True,
+        verbose=True,
+        temperature=0.8,
+        callbacks=[callback, extra_callback],
+    )
 
-#     def on_chain_end(self, outputs: Dict[str, Any], **kwargs: Any) -> None:
-#         """Run when chain ends running."""
+    async def wrap_done(fn: Awaitable, event: asyncio.Event):
+        """Wrap an awaitable with a event to signal when it's done or an exception is raised."""
+        try:
+            await fn
+        except Exception as e:
+            print(f"Caught exception: {e}")
+        finally:
+            # Signal the aiter to stop.
+            event.set()
+    # Begin a task that runs in the background.
+    task = asyncio.create_task(wrap_done(
+        model.agenerate(messages=[[HumanMessage(content=message)]]),
+        callback.done),
+    )
 
-#     def on_chain_error(
-#         self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
-#     ) -> None:
-#         """Run when chain errors."""
-
-#     def on_tool_start(
-#         self, serialized: Dict[str, Any], input_str: str, **kwargs: Any
-#     ) -> None:
-#         """Run when tool starts running."""
-
-#     def on_agent_action(self, action: AgentAction, **kwargs: Any) -> Any:
-#         """Run on agent action."""
-#         pass
-
-#     def on_tool_end(self, output: str, **kwargs: Any) -> None:
-#         """Run when tool ends running."""
-
-#     def on_tool_error(
-#         self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
-#     ) -> None:
-#         """Run when tool errors."""
-
-#     def on_text(self, text: str, **kwargs: Any) -> None:
-#         """Run on arbitrary text."""
-
-#     def on_agent_finish(self, finish: AgentFinish, **kwargs: Any) -> None:
-#         """Run on agent end."""
-
-# class QuestionGenCallbackHandler(AsyncCallbackHandler):
-#     """Callback handler for question generation."""
-
-#     def __init__(self, websocket):
-#         self.websocket = websocket
-
-#     async def on_llm_start(
-#         self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any
-#     ) -> None:
-#         """Run when LLM starts running."""
-#         resp = ChatResponse(
-#             sender="bot", message="Synthesizing question...", type="info"
-#         )
-#         await self.websocket.send_json(resp.dict())
+    async for token in callback.aiter():
+        # Use server-sent-events to stream the response
+        yield f"{token}"
+    await task
 
 
 class ChatResponse(BaseModel):
@@ -235,3 +228,16 @@ class ChatResponse(BaseModel):
         if v not in ["start", "stream", "end", "error", "info"]:
             raise ValueError("type must be start, stream or end")
         return v
+
+
+class StreamRequest(BaseModel):
+    """Request body for streaming."""
+    prompt: str
+    chat_group_id: str
+    memory_size: Optional[int] = None
+
+
+class ChatMessage(BaseModel):
+    chat_history: List[str]
+    prompt: str
+    chat_id: Optional[Union[str, None]] = None
